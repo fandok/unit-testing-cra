dc7f405cd112713acd77579489e65ca683629b8e
fandok <fandika.okdiba@gmail.com>
2023-05-29 23:04:01 +0700

    add unit test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,23 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { fireEvent, render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+test("renders and get edit text", () => {
+  render(<App />);
+  const editText = screen.getByText(/save to reload/);
+  expect(editText).toBeInTheDocument();
+});
+
+test("renders and can click button", () => {
+  console.log = jest.fn();
+  render(<App />);
+  const buttonComponent = screen.getByText("Bisa diklik");
+  expect(buttonComponent).toBeInTheDocument();
+  fireEvent.click(buttonComponent);
+  expect(console.log).toHaveBeenCalledWith("test");
 });
